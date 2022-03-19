6c65d42d80ddcfa67f8c50e01558880f31c93273
Lukáš Hozda <luk.hozda@gmail.com>
2022-03-20 00:05:37 +0100

    kameli: Create register_all() helper function

--- src/snippets/mod.rs
+++ src/snippets/mod.rs
@@ -2,3 +2,12 @@
 
 pub mod haskell;
 pub mod nomeme;
+
+/// Register every service currently available with default configuration
+pub fn register_all<T>(executor: &T)
+where
+    T: crate::Executor,
+{
+    executor.register(Box::new(haskell::Haskell));
+    executor.register(Box::new(nomeme::NoMeme::new()));
+}
