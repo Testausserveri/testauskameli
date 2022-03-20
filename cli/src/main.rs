e23ade41f8fc58d492fe69a9c40f48622542bdd6
Lukáš Hozda <luk.hozda@gmail.com>
2022-03-20 16:02:22 +0100

    cli: Implement cli frontend

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
+use tokio::fs::read_to_string;
+use tokio::io::AsyncReadExt;
 use tracing::*;
 
 use testauskameli::snippets::register_all;
@@ -16,10 +20,31 @@ async fn main() {
     register_all(&executor);
     info!("executor started");
 
-    let (_sender, receiver) = flume::unbounded();
-    tokio::spawn(async move { executor.run(receiver).await });
+    let (sender, receiver) = flume::unbounded();
+    let task = tokio::spawn(async move { executor.run(receiver).await });
 
-    loop {
-        todo!("Get and send input");
-    }
+    let arg = env::args().next();
+
+    let input = match (&arg).into_iter().map(|x| x.as_ref()).next() {
+        None | Some("-") => {
+            let mut stdin = tokio::io::stdin();
+            let mut input = String::new();
+
+            stdin
+                .read_to_string(&mut input)
+                .await
+                .expect("failed to read from stdin");
+            input
+        }
+        Some(file) => read_to_string(file).await.expect("failed to read file"),
+    };
+
+    sender
+        .send_async((input, ()))
+        .await
+        .expect("BUG: impossible bruh");
+
+    drop(sender);
+
+    task.await.unwrap();
 }
