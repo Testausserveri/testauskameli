51b353790b4ed3fa207b643f3422727a2b0c98fa
PEEVEE <peeveezz2020@gmail.com>
2022-03-22 16:57:20 +0200

    started implementing nodejs

--- testauskameli/src/snippets/mod.rs
+++ testauskameli/src/snippets/mod.rs
@@ -4,6 +4,7 @@ pub mod c;
 pub mod echo;
 pub mod haskell;
 pub mod nomeme;
+pub mod js;
 
 /// Register every service currently available with default configuration
 pub fn register_all<T>(executor: &T)
@@ -11,6 +12,7 @@ where
     T: crate::Executor,
 {
     executor.register(Box::new(c::C));
+    executor.register(Box::new(js::JS));
     executor.register(Box::new(haskell::Haskell));
     executor.register(Box::new(nomeme::NoMeme::new()));
     executor.register(Box::new(echo::Echo));
